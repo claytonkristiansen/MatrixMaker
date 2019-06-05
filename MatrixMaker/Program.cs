09191430c8d3018eb0394904ab6de9aa18ae9355
Clayton Kristiansen <kristiansenc@rsd407.org>
2019-06-04 20:40:30 -0700

    file input

--- MatrixMaker/Program.cs
+++ MatrixMaker/Program.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace MatrixMaker
 {
     class Program
     {
+        public static List<SchoolEntry> ParseSchoolEntriesCSV(String filePath)
+        {
+            List<SchoolEntry> list = new List<SchoolEntry>();
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                while(!reader.EndOfStream)
+                {
+                    String[] result = reader.ReadLine().Split(',');
+                    list.Add(new SchoolEntry(result[0], result[1], float.Parse(result[2])));
+                }
+            }
+            return list;
+        }
+
         static void Main(string[] args)
         {
+            List<SchoolEntry> list = ParseSchoolEntriesCSV("Assets/CollegeData.csv");
             Console.WriteLine("Hello World!");
         }
     }
---
+++ MatrixMaker/SchoolEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixMaker
+{
+    class SchoolEntry
+    {
+        String name;
+        String region;
+        float tuition;
+        public SchoolEntry(String _name, String _region, float _tuition)
+        {
+            name = _name;
+            region = _region;
+            tuition = _tuition;
+        }
+    }
+}
